b741ab2ac2f48b72e326b511e192b352ad9f768b
John Samuel <jsamchineme@gmail.com>
2018-10-27 07:14:52 +0100

    Remove local test script for circleci

--- .circleci/run-build-locally.sh
+++
@@ -1,6 +0,0 @@
-curl --user 28ce1efadd26e4ccbf7975e0b77acf3c24e8fb5e: \
-    --request POST \
-    --form revision=eaef222493d41eae0636d2d319b7a9b11eca0856\
-    --form config=@config.yml \
-    --form notify=false \
-        https://circleci.com/api/v1.1/project/github/jsamchineme/tdd-showcase/tree/master
